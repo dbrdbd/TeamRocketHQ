f80468e09a298953fd6815b1154108819773c9b4
Sean McLoughlin <SeanMcLoughlin@users.noreply.github.com>
2015-01-17 11:11:58 -0500

    Update Swapper.cs
    Vector3 not working

--- Swapper.cs
+++ Swapper.cs
@@ -2,18 +2,39 @@ using UnityEngine;
 using System.Collections;
 
 public class Swapper : MonoBehaviour {
-
-	void Start()
-	{
-		
-	}
 	
-	void Swap()
+	void Update()
 	{
-		GameObject object1;
-		GameObject object2;
-		Vector3 tempPosition = object1.transform.position;
-		object1.transform.position = object2.transform.position;
-		object2.transform.position = tempPosition;
+		GameObject object1 = GameObject.Find("25"), object2;
+		if (Input.GetButtonDown("W"))
+		{
+			object2 = GameObject.Find("18");
+			Vector3 tempPosition = object1.transform.position;
+			object1.transform.position = object2.transform.position;
+			object2.transform.position = tempPosition;
+		}
+			
+		else if (Input.GetButtonDown("A"))
+		{
+			object2 = GameObject.Find("24");
+			Vector3 tempPosition = object1.transform.position;
+			object1.transform.position = object2.transform.position;
+			object2.transform.position = tempPosition;
+		}
+		else if (Input.GetButtonDown("S"))
+		{
+			object2 = GameObject.Find("32");
+			Vector3 tempPosition = object1.transform.position;
+			object1.transform.position = object2.transform.position;
+			object2.transform.position = tempPosition;
+		}
+		else if (Input.GetButtonDown("D"))
+		{
+			object2 = GameObject.Find("26")
+			Vector3 tempPosition = object1.transform.position;
+			object1.transform.position = object2.transform.position;
+			object2.transform.position = tempPosition;
+		}
 	}
+
 }
