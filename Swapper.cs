7d0c6098433140933b35f7b5a675070a3d79a1f4
Sean McLoughlin <SeanMcLoughlin@users.noreply.github.com>
2015-01-17 12:57:10 -0500

    Update Swapper.cs

--- Swapper.cs
+++ Swapper.cs
@@ -2,34 +2,39 @@ using UnityEngine;
 using System.Collections;
 
 public class Swapper : MonoBehaviour {
-	public GameObject object1, object2;
-	public Vector3 temp;
+	private GameObject object1, object2;
+	private Vector3 temp;
 	void Update()
 	{
 		object1 = GameObject.Find ("25");
-		if (Input.GetButtonDown("W"))
+		//print (object1);
+		if (Input.GetKeyDown(KeyCode.W))
 		{
 			object2 = GameObject.Find("18");
+			//print ("Object 2 position: " + object2.transform.position.ToString());
 			temp = object1.transform.position;
+			//print ("Temp: " + temp.ToString ());
 			object1.transform.position = object2.transform.position;
+			//print ("Object 1 Position: " + object1.transform.position.ToString ());
 			object2.transform.position = temp;
+			//print ("Object 2 Position: " + object2.transform.position.ToString ());
 		}
 			
-		else if (Input.GetButtonDown("A"))
+		else if (Input.GetKeyDown(KeyCode.A))
 		{
 			object2 = GameObject.Find("24");
 			temp = object1.transform.position;
 			object1.transform.position = object2.transform.position;
 			object2.transform.position = temp;
 		}
-		else if (Input.GetButtonDown("S"))
+		else if (Input.GetKeyDown(KeyCode.S))
 		{
 			object2 = GameObject.Find("32");
 			temp = object1.transform.position;
 			object1.transform.position = object2.transform.position;
 			object2.transform.position = temp;
 		}
-		else if (Input.GetButtonDown("D"))
+		else if (Input.GetKeyDown(KeyCode.D))
 		{
 			object2 = GameObject.Find("26");
 			temp = object1.transform.position;
